229b792e25cdb290aec92d59e5d366274774c260
tracer-nju <tracer@nju.edu.cn>
2022-05-08 03:02:05 -0700

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:02:05 up 1 day, 14:21,  2 users,  load average: 0.61, 1.11, 0.80

--- amgame/src/keyboard.c
+++ amgame/src/keyboard.c
@@ -5,11 +5,8 @@
 static const char *key_names[] = {
     AM_KEYS(KEYNAME)};
 
-static int x, y = 0;
-
 void print_key()
 {
-  int SIDE = 16;
   AM_INPUT_KEYBRD_T event = {.keycode = AM_KEY_NONE};
   ioe_read(AM_INPUT_KEYBRD, &event);
   if (event.keycode != AM_KEY_NONE && event.keydown)
@@ -17,38 +14,43 @@ void print_key()
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\r\n");
-    if (strcmp(key_names[event.keycode], "W") == 0)
-    {
-      puts("pressed W \r\n");
-      if (y > 0)
-      {
-        y--;
-      }
-    }
-    else if (strcmp(key_names[event.keycode], "A") == 0)
-    {
-      puts("pressed A \r\n");
-      if (x > 0)
-      {
-        x--;
-      }
-    }
-    else if (strcmp(key_names[event.keycode], "S") == 0)
-    {
-      puts("pressed S \r\n");
-      if (y < 29)
-      {
-        y++;
-      }
-    }
-    else if (strcmp(key_names[event.keycode], "D") == 0)
-    {
-      puts("pressed D \r\n");
-      if (x < 39)
-      {
-        x++;
-      }
-    }
-    draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x00ff99);
   }
 }
+
+
+
+// void test() {
+//   if (strcmp(key_names[event.keycode], "W") == 0)
+//     {
+//       puts("pressed W \r\n");
+//       if (y > 0)
+//       {
+//         y--;
+//       }
+//     }
+//     else if (strcmp(key_names[event.keycode], "A") == 0)
+//     {
+//       puts("pressed A \r\n");
+//       if (x > 0)
+//       {
+//         x--;
+//       }
+//     }
+//     else if (strcmp(key_names[event.keycode], "S") == 0)
+//     {
+//       puts("pressed S \r\n");
+//       if (y < 29)
+//       {
+//         y++;
+//       }
+//     }
+//     else if (strcmp(key_names[event.keycode], "D") == 0)
+//     {
+//       puts("pressed D \r\n");
+//       if (x < 39)
+//       {
+//         x++;
+//       }
+//     }
+//     draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x00ff99);
+// }
