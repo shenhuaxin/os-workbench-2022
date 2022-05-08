034c77ae43d06ce7d79ba5d56fd1920d00e5d0ef
tracer-nju <tracer@nju.edu.cn>
2022-05-08 02:20:02 -0700

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:20:02 up 1 day, 13:39,  2 users,  load average: 0.15, 0.30, 0.45

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,7 +1,7 @@
 #include <game.h>
 #include <unistd.h>
 #define FPS          60
-#define SIDE 16
+#define SIDE 4
 
 // Operating system is a C program!
 int main(const char *args)
@@ -15,13 +15,19 @@ int main(const char *args)
   splash();
 
   puts("Press any key to see its key code...\n");
+  int x = 0, y = 0;
+  int vy = 60;
+  int vx = 0;
   while (1)
   {
     // 在每一个时间片，例如每 16.7ms (60 fps)
     wait_for_frame();
     splash();
     // 做完这一个时间片内需要完成的工作
-    draw_tile(0, 0, SIDE, SIDE, 0xFFA500);
+    draw_tile(x, x, SIDE, SIDE, 0xffffff);
+    x = x + vx/FPS;
+    y = y + vy/FPS;
+    draw_tile(x, x, SIDE, SIDE, 0xFFA500);
   }
   return 0;
 }
