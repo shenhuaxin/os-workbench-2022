19c46f9e1a8721cbd61eac63f28df30709cfac6c
tracer-nju <tracer@nju.edu.cn>
2022-05-08 02:03:15 -0700

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:03:15 up 1 day, 13:22,  2 users,  load average: 0.06, 0.23, 0.19

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -14,21 +14,18 @@ int main(const char *args)
   splash();
 
   puts("Press any key to see its key code...\n");
-  uint32_t color = 0x00ff00;
   while (1)
   {
     // 在每一个时间片，例如每 16.7ms (60 fps)
     wait_for_frame();
 
     // 做完这一个时间片内需要完成的工作
-    // int scanlines = 262;
-    // while (scanlines-- > 0)
-    // {
-    //   ppu_cycle();      // 更新游戏图像
-    //   psg_detect_key(); // 读取按键，更新游戏逻辑
-    // }
-    color = color + 100;
-    splash_color(color);
+    int scanlines = 262;
+    while (scanlines-- > 0)
+    {
+      // ppu_cycle();      // 更新游戏图像
+      // psg_detect_key(); // 读取按键，更新游戏逻辑
+    }
   }
   return 0;
 }
